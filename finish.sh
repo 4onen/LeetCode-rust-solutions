90d2289a5d0c529c70bf5ff80b8bad70465b1572
4onen <mattjdupree@gmail.com>
2024-01-17 17:16:38 -0800

    Add source control scripts.

---
+++ finish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+FILES=( $(git status --porcelain | grep -P ' src/(?!lib.rs)' | cut -d'/' -f2 | tr '\n' ' ') );
+
+if [ -z "${FILES[@]}" ]; then
+    echo "No files detected"
+    exit 1
+fi
+
+echo "Detected files: ${FILES[@]}"
+echo "Testing..."
+
+for file in ${FILES[@]}; do
+    if ! cargo test ${file%.rs}; then
+        echo "Test failed for $file"
+        exit 1
+    fi
+done
+
+git add src/lib.rs ${FILES[@]/#/src/}
+git commit -m "Completed $(echo ${FILES[@]%.rs} | tr ' ' ', ')"
