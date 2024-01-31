dcfd2ae61c635c4fe602e557cb86b3e8337fd6d5
4onen <mattjdupree@gmail.com>
2024-01-30 19:14:39 -0800

    Hyper-optimize daily_temperatures.

--- src/daily_temperatures.rs
+++ src/daily_temperatures.rs
@@ -21,17 +21,130 @@ pub struct Solution;
 // }
 
 // Lookup-avoidant stack-based sol'n
+// impl Solution {
+//     pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+//         let mut stack: Vec<(u32, u8)> = vec![];
+//         let mut result = vec![0; temperatures.len()];
+//         for i in 0..temperatures.len() as u32 {
+//             let this_temp = temperatures[i as usize] as u8;
+//             while !stack.is_empty() && this_temp > stack.last().unwrap().1 {
+//                 let (idx, _) = stack.pop().unwrap();
+//                 result[idx as usize] = (i - idx) as i32;
+//             }
+//             stack.push((i, this_temp));
+//         }
+//         result
+//     }
+// }
+
+// Further optimized sol'n
+// impl Solution {
+//     pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+//         let mut stack: Vec<(u32, u8)> = vec![];
+//         let mut result = vec![0; temperatures.len()];
+//         for i in 0..temperatures.len() as u32 {
+//             let this_temp = temperatures[i as usize] as u8;
+//             if this_temp >= 100 {
+//                 for (idx, _) in stack.drain(..) {
+//                     result[idx as usize] = (i - idx) as i32;
+//                 }
+//                 // i remains at 0 because no day is hotter than 100
+//             } else {
+//                 let mut j = stack.len();
+//                 while j > 0 && this_temp > stack[j - 1].1 {
+//                     let idx = stack[j - 1].0;
+//                     result[idx as usize] = (i - idx) as i32;
+//                     j -= 1;
+//                 }
+//                 stack.truncate(j);
+//                 stack.push((i, this_temp));
+//             }
+//         }
+//         result
+//     }
+// }
+
+// Reverse iteration sol'n
+// impl Solution {
+//     pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+//         let mut stack: Vec<u32> = vec![];
+//         let mut result: Vec<i32> = vec![0; temperatures.len()];
+//         for i in (0..temperatures.len() as u32).rev() {
+//             let this_temp = temperatures[i as usize];
+//             let mut j = stack.len() as u32;
+//             while j > 0 && this_temp >= temperatures[stack[(j - 1) as usize] as usize] {
+//                 j -= 1;
+//             }
+//             stack.truncate(j as usize);
+//             result[i as usize] = if let Some(&idx) = stack.last() {
+//                 (idx - i) as i32
+//             } else {
+//                 0
+//             };
+//             stack.push(i);
+//         }
+//         result
+//     }
+// }
+
+// Reverse iteration lookup avoidant sol'n
+// impl Solution {
+//     pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+//         let mut stack: Vec<(u32, u8)> = vec![];
+//         let mut result: Vec<i32> = vec![0; temperatures.len()];
+//         for i in (0..temperatures.len() as u32).rev() {
+//             let this_temp = temperatures[i as usize] as u8;
+//             let mut j = stack.len() as u32;
+//             while j > 0 && this_temp >= stack[(j - 1) as usize].1 {
+//                 j -= 1;
+//             }
+//             stack.truncate(j as usize);
+//             result[i as usize] = if let Some(&(idx, _)) = stack.last() {
+//                 (idx - i) as i32
+//             } else {
+//                 0
+//             };
+//             stack.push((i, this_temp));
+//         }
+//         result
+//     }
+// }
+
+// Reverse iteration bit packed sol'n
 impl Solution {
     pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
-        let mut stack: Vec<(u32, u8)> = vec![];
-        let mut result = vec![0; temperatures.len()];
-        for i in 0..temperatures.len() as u32 {
-            let this_temp = temperatures[i as usize] as u8;
-            while !stack.is_empty() && this_temp > stack.last().unwrap().1 {
-                let idx = stack.pop().unwrap().0;
-                result[idx as usize] = (i - idx) as i32;
+        #[derive(Debug, Clone, Copy)]
+        struct StackEntry {
+            pack: u32, // idx: u24 upper, temp: u8 lower
+        }
+        impl StackEntry {
+            fn new(idx: u32, temp: u8) -> Self {
+                Self {
+                    pack: (idx << 8) | temp as u32,
+                }
             }
-            stack.push((i, this_temp));
+            fn idx(&self) -> u32 {
+                self.pack >> 8
+            }
+            fn temp(&self) -> u8 {
+                self.pack as u8
+            }
+        }
+        let mut stack: Vec<StackEntry> = vec![];
+        let mut result: Vec<i32> = vec![0; temperatures.len()];
+        for i in (0..temperatures.len() as u32).rev() {
+            let this_temp = temperatures[i as usize] as u8;
+            let mut j = stack.len() as u32;
+            while j > 0 && this_temp >= stack[(j - 1) as usize].temp() {
+                j -= 1;
+            }
+            stack.truncate(j as usize);
+            result[i as usize] = if let Some(&entry) = stack.last() {
+                (entry.idx() - i) as i32
+            } else {
+                0
+            };
+            stack.push(StackEntry::new(i, this_temp));
         }
         result
     }
@@ -116,6 +229,14 @@ mod tests {
             differing_index_ranges.is_empty(),
             "Differing indices: {:?}",
             differing_index_ranges
+        );
+    }
+
+    #[test]
+    fn myex1() {
+        assert_eq!(
+            Solution::daily_temperatures(vec![30, 30, 30, 31, 31, 32, 32]),
+            vec![3, 2, 1, 2, 1, 0, 0]
         );
     }
 }
