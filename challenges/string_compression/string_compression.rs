a6a9d244df82c65903b2d30ce8e59a4365d6a794
Matthew Dupree <mattjdupree@gmail.com>
2024-04-16 21:57:32 -0700

    Add solution for string_compression

--- challenges/string_compression/string_compression.rs
+++ challenges/string_compression/string_compression.rs
@@ -2,35 +2,75 @@
 
 pub struct Solution;
 
+// Initial sol'n
+// impl Solution {
+//     pub fn compress(chars: &mut Vec<char>) -> i32 {
+//         let mut original_idx = 0;
+//         let mut new_idx = 0;
+//         while original_idx < chars.len() {
+//             let mut original_idx2 = original_idx;
+//             while original_idx2 < chars.len() && chars[original_idx2] == chars[original_idx] {
+//                 original_idx2 += 1;
+//             }
+//             chars[new_idx] = chars[original_idx];
+//             new_idx += 1;
+//             if original_idx2 - original_idx > 1 {
+//                 let mut count = original_idx2 - original_idx;
+//                 let mut count_digits = 0;
+//                 while count > 0 {
+//                     count /= 10;
+//                     count_digits += 1;
+//                 }
+//                 let mut count = original_idx2 - original_idx;
+//                 for i in (0..count_digits).rev() {
+//                     chars[new_idx + i] = ((count % 10) as u8 + 48u8) as char;
+//                     count /= 10;
+//                 }
+//                 new_idx += count_digits;
+//             }
+//             original_idx = original_idx2;
+//         }
+//         unsafe { chars.set_len(new_idx) };
+//         new_idx as i32
+//     }
+// }
+
+// Updated, faster sol'n
 impl Solution {
     pub fn compress(chars: &mut Vec<char>) -> i32 {
-        let mut original_idx = 0;
-        let mut new_idx = 0;
-        while original_idx < chars.len() {
-            let mut original_idx2 = original_idx;
-            while original_idx2 < chars.len() && chars[original_idx2] == chars[original_idx] {
-                original_idx2 += 1;
+        fn write_count(chars: &mut Vec<char>, count: u16, new_len: &mut u16) {
+            if count < 2 {
+                return;
             }
-            chars[new_idx] = chars[original_idx];
-            new_idx += 1;
-            if original_idx2 - original_idx > 1 {
-                let mut count = original_idx2 - original_idx;
-                let mut count_digits = 0;
-                while count > 0 {
-                    count /= 10;
-                    count_digits += 1;
-                }
-                let mut count = original_idx2 - original_idx;
-                for i in (0..count_digits).rev() {
-                    chars[new_idx + i] = ((count % 10) as u8 + 48u8) as char;
-                    count /= 10;
-                }
-                new_idx += count_digits;
+            let mut digit_count = count;
+            let mut count_digits = 0;
+            while digit_count > 0 {
+                digit_count /= 10;
+                count_digits += 1;
             }
-            original_idx = original_idx2;
+            let mut count = count;
+            for i in (0..count_digits).rev() {
+                chars[*new_len as usize + i as usize] = ((count % 10) as u8 + 48u8) as char;
+                count /= 10;
+            }
+            *new_len += count_digits;
         }
-        unsafe { chars.set_len(new_idx) };
-        new_idx as i32
+        let mut new_len = 0u16;
+        let mut last_idx = 0u16;
+        for original_idx in 0..chars.len() as u16 {
+            if chars[last_idx as usize] != chars[original_idx as usize] {
+                chars[new_len as usize] = chars[last_idx as usize];
+                new_len += 1;
+                write_count(chars, original_idx - last_idx, &mut new_len);
+                last_idx = original_idx;
+            }
+        }
+        // Write the last character
+        chars[new_len as usize] = chars[last_idx as usize];
+        new_len += 1;
+        write_count(chars, chars.len() as u16 - last_idx, &mut new_len);
+        unsafe { chars.set_len(new_len as usize) };
+        new_len as i32
     }
 }
 
@@ -38,40 +78,48 @@ impl Solution {
 mod tests {
     use super::*;
 
+    fn test(chars: &[char], expected: &[char]) {
+        let mut chars = chars.to_vec();
+        assert_eq!(Solution::compress(&mut chars), expected.len() as i32);
+        assert_eq!(chars, expected);
+    }
+
     #[test]
     fn ex1() {
-        let mut chars = vec!['a', 'a', 'b', 'b', 'c', 'c', 'c'];
-        assert_eq!(Solution::compress(&mut chars), 6);
-        assert_eq!(chars, vec!['a', '2', 'b', '2', 'c', '3']);
+        test(
+            &['a', 'a', 'b', 'b', 'c', 'c', 'c'],
+            &['a', '2', 'b', '2', 'c', '3'],
+        )
     }
 
     #[test]
     fn ex2() {
-        let mut chars = vec!['a'];
-        assert_eq!(Solution::compress(&mut chars), 1);
-        assert_eq!(chars, vec!['a']);
+        test(&['a'], &['a'])
     }
 
     #[test]
     fn ex3() {
-        let mut chars = vec![
-            'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b',
-        ];
-        assert_eq!(Solution::compress(&mut chars), 4);
-        assert_eq!(chars, vec!['a', 'b', '1', '2']);
+        test(
+            &[
+                'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b',
+            ],
+            &['a', 'b', '1', '2'],
+        )
     }
 
     #[test]
     fn ex4() {
-        let mut chars = vec!['a', 'a', 'a', 'b', 'b', 'a', 'a'];
-        assert_eq!(Solution::compress(&mut chars), 6);
-        assert_eq!(chars, vec!['a', '3', 'b', '2', 'a', '2']);
+        test(
+            &['a', 'a', 'a', 'b', 'b', 'a', 'a'],
+            &['a', '3', 'b', '2', 'a', '2'],
+        )
     }
 
     #[test]
     fn myex1() {
-        let mut chars = vec!['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'];
-        assert_eq!(Solution::compress(&mut chars), 3);
-        assert_eq!(chars, vec!['a', '1', '0']);
+        test(
+            &['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+            &['a', '1', '0'],
+        )
     }
 }
