1fbde43895de4ac2a0a06fbb33df6f54f0c4d9bb
Matthew Dupree <mattjdupree@gmail.com>
2024-05-07 21:04:05 -0700

    Add solution for relative_ranks

--- challenges/relative_ranks/relative_ranks.rs
+++ challenges/relative_ranks/relative_ranks.rs
@@ -2,27 +2,52 @@
 
 pub struct Solution;
 
+// Initial sol'n
+// impl Solution {
+//     pub fn find_relative_ranks(score: Vec<i32>) -> Vec<String> {
+//         let mut sorted_score = score
+//             .iter()
+//             .copied()
+//             .collect::<std::collections::BinaryHeap<_>>();
+//         let mut places: std::collections::HashMap<i32, u16> = std::collections::HashMap::new();
+//         let mut place = unsafe { std::num::NonZeroU16::new_unchecked(1) };
+//         while let Some(score) = sorted_score.pop() {
+//             places.insert(score, place.get());
+//             place = place.checked_add(1).unwrap();
+//         }
+//         score
+//             .into_iter()
+//             .map(|score| match places[&score] {
+//                 1 => "Gold Medal".to_string(),
+//                 2 => "Silver Medal".to_string(),
+//                 3 => "Bronze Medal".to_string(),
+//                 place => place.to_string(),
+//             })
+//             .collect()
+//     }
+// }
+
+// One array copy sol'n
 impl Solution {
     pub fn find_relative_ranks(score: Vec<i32>) -> Vec<String> {
-        let mut sorted_score = score
-            .iter()
-            .copied()
-            .collect::<std::collections::BinaryHeap<_>>();
-        let mut places: std::collections::HashMap<i32, u16> = std::collections::HashMap::new();
-        let mut place = unsafe { std::num::NonZeroU16::new_unchecked(1) };
-        while let Some(score) = sorted_score.pop() {
-            places.insert(score, place.get());
-            place = place.checked_add(1).unwrap();
+        assert!(score.len() > 0);
+        assert!(score.len() <= 10000);
+        let sorted_score = {
+            let mut sorted_score = score.clone();
+            sorted_score.sort_unstable();
+            sorted_score
+        };
+        let mut ret = Vec::with_capacity(score.len());
+        for score in score {
+            match sorted_score.len() - sorted_score.binary_search(&score).unwrap() {
+                0 => unreachable!("score not found in sorted_score"),
+                1 => ret.push("Gold Medal".to_string()),
+                2 => ret.push("Silver Medal".to_string()),
+                3 => ret.push("Bronze Medal".to_string()),
+                place => ret.push(place.to_string()),
+            }
         }
-        score
-            .into_iter()
-            .map(|score| match places[&score] {
-                1 => "Gold Medal".to_string(),
-                2 => "Silver Medal".to_string(),
-                3 => "Bronze Medal".to_string(),
-                place => place.to_string(),
-            })
-            .collect()
+        ret
     }
 }
 
