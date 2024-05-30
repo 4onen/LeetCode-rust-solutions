54936c329b07d5caa087cc3e04e9866e55cc88f3
4onen <mattjdupree@gmail.com>
2024-05-29 17:44:26 -0700

    Add solution for count_triplets_that_can_form_two_arrays_of_equal_xor

---
+++ challenges/count_triplets_that_can_form_two_arrays_of_equal_xor/count_triplets_that_can_form_two_arrays_of_equal_xor.rs
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/
+
+pub struct Solution;
+
+impl Solution {
+    pub fn count_triplets(arr: Vec<i32>) -> i32 {
+        let mut count: i32 = 0;
+        for i in 0..arr.len() as u16 {
+            let mut partial = arr[i as usize];
+            for j in (i + 1)..arr.len() as u16 {
+                partial ^= arr[j as usize];
+                if partial == 0 {
+                    count += j as i32 - i as i32;
+                }
+            }
+        }
+        count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test(arr: &[i32], expected: i32) {
+        assert!(arr.len() >= 1);
+        assert!(arr.len() <= 300);
+        assert_eq!(Solution::count_triplets(arr.to_vec()), expected);
+    }
+
+    #[test]
+    fn ex1() {
+        test(&[2, 3, 1, 6, 7], 4);
+    }
+
+    #[test]
+    fn ex2() {
+        test(&[1, 1, 1, 1, 1], 10);
+    }
+
+    #[test]
+    fn discussion_case1() {
+        test(
+            &[
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+            ],
+            2250050,
+        );
+    }
+}
