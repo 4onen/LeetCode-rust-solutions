ce7450f476af94cb93b7881d64e271b53ae99387
4onen <mattjdupree@gmail.com>
2024-06-24 19:19:17 -0500

    Add solution for binary_search_tree_to_greater_sum_tree

---
+++ challenges/binary_search_tree_to_greater_sum_tree/binary_search_tree_to_greater_sum_tree.rs
@@ -0,0 +1,86 @@
+// https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+use std::cell::RefCell;
+use std::rc::Rc;
+use utils::TreeNode;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn bst_to_gst(mut root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        fn right_first_traverse(
+            node: &mut Option<Rc<RefCell<TreeNode>>>,
+            sum: &mut i32,
+        ) {
+            if let Some(n) = node.as_mut() {
+                let mut n = n.borrow_mut();
+                right_first_traverse(&mut n.right, sum);
+                *sum += n.val;
+                n.val = *sum;
+                right_first_traverse(&mut n.left, sum);
+            }
+        }
+        let mut sum = 0;
+        right_first_traverse(&mut root, &mut sum);
+        root
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test(input: &[Option<i32>], output: &[Option<i32>]) {
+        assert_eq!(
+            Solution::bst_to_gst(TreeNode::from_leetcode_slice(input)),
+            TreeNode::from_leetcode_slice(output)
+        );
+    }
+
+    #[test]
+    fn ex1() {
+        test(
+            &[
+                Some(4),
+                Some(1),
+                Some(6),
+                Some(0),
+                Some(2),
+                Some(5),
+                Some(7),
+                None,
+                None,
+                None,
+                Some(3),
+                None,
+                None,
+                None,
+                Some(8),
+            ],
+            &[
+                Some(30),
+                Some(36),
+                Some(21),
+                Some(36),
+                Some(35),
+                Some(26),
+                Some(15),
+                None,
+                None,
+                None,
+                Some(33),
+                None,
+                None,
+                None,
+                Some(8),
+            ],
+        )
+    }
+
+    #[test]
+    fn ex2() {
+        test(
+            &[Some(0), None, Some(1)],
+            &[Some(1), None, Some(1)],
+        )
+    }
+}
