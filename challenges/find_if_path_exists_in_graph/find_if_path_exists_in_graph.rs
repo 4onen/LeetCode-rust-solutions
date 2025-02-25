8b1727596f966417f753fe94fadccc8f1c843ebe
4onen <mattjdupree@gmail.com>
2025-02-24 20:46:14 -0800

    Add solution for find_if_path_exists_in_graph

--- challenges/find_if_path_exists_in_graph/find_if_path_exists_in_graph.rs
+++ challenges/find_if_path_exists_in_graph/find_if_path_exists_in_graph.rs
@@ -2,33 +2,75 @@
 
 pub struct Solution;
 
+// Initial sol'n - Union-Find but not doing smart things like path compression
+// impl Solution {
+//     pub fn valid_path(n: i32, edges: Vec<Vec<i32>>, source: i32, destination: i32) -> bool {
+//         type UnionFindItem = u32;
+//         struct UnionFind {
+//             parent: Vec<UnionFindItem>,
+//             // No rank -- It's already fast enough
+//         }
+//         impl UnionFind {
+//             fn new(n: UnionFindItem) -> Self {
+//                 UnionFind {
+//                     parent: (0..n).collect(),
+//                 }
+//             }
+//             fn find(&mut self, mut x: UnionFindItem) -> UnionFindItem {
+//                 while x != self.parent[x as usize] {
+//                     self.parent[x as usize] = self.parent[self.parent[x as usize] as usize];
+//                     x = self.parent[x as usize];
+//                 }
+//                 x
+//             }
+//             fn union(&mut self, x: UnionFindItem, y: UnionFindItem) {
+//                 let root_x = self.find(x);
+//                 let root_y = self.find(y);
+//                 if root_x != root_y {
+//                     self.parent[root_x as usize] = root_y;
+//                 }
+//             }
+//         }
+//         assert!(n > 0);
+//         assert!(n <= 2 * (10i32.pow(5)));
+//         assert!(source >= 0);
+//         assert!(source < n);
+//         assert!(destination >= 0);
+//         assert!(destination < n);
+//         let n = n as UnionFindItem;
+//         let source = source as UnionFindItem;
+//         let destination = destination as UnionFindItem;
+//         let mut uf = UnionFind::new(n);
+//         for edge in edges {
+//             let u = edge[0];
+//             if u < 0 {
+//                 unreachable!("Invalid negative edge: {:?}", edge);
+//             }
+//             let u = u as UnionFindItem;
+//             let v = edge[1];
+//             if v < 0 {
+//                 unreachable!("Invalid negative edge: {:?}", edge);
+//             }
+//             let v = v as UnionFindItem;
+//             uf.union(u, v);
+//         }
+//         uf.find(source) == uf.find(destination)
+//     }
+// }
+
+// Optimized sol'n
 impl Solution {
     pub fn valid_path(n: i32, edges: Vec<Vec<i32>>, source: i32, destination: i32) -> bool {
         type UnionFindItem = u32;
-        struct UnionFind {
-            parent: Vec<UnionFindItem>,
-            // No rank -- It's already fast enough
-        }
-        impl UnionFind {
-            fn new(n: UnionFindItem) -> Self {
-                UnionFind {
-                    parent: (0..n).collect(),
-                }
+        // Can be const in rustc 1.84.0 (9fc6b4312 2025-01-07)
+        const fn find(uf: &mut [UnionFindItem], mut x: UnionFindItem) -> UnionFindItem {
+            while uf[x as usize] != x as UnionFindItem {
+                let new = uf[x as usize];
+                let new_new = uf[new as usize];
+                uf[x as usize] = new_new;
+                x = new_new;
             }
-            fn find(&mut self, mut x: UnionFindItem) -> UnionFindItem {
-                while x != self.parent[x as usize] {
-                    self.parent[x as usize] = self.parent[self.parent[x as usize] as usize];
-                    x = self.parent[x as usize];
-                }
-                x
-            }
-            fn union(&mut self, x: UnionFindItem, y: UnionFindItem) {
-                let root_x = self.find(x);
-                let root_y = self.find(y);
-                if root_x != root_y {
-                    self.parent[root_x as usize] = root_y;
-                }
-            }
+            x
         }
         assert!(n > 0);
         assert!(n <= 2 * (10i32.pow(5)));
@@ -37,23 +79,16 @@ impl Solution {
         assert!(destination >= 0);
         assert!(destination < n);
         let n = n as UnionFindItem;
-        let source = source as UnionFindItem;
-        let destination = destination as UnionFindItem;
-        let mut uf = UnionFind::new(n);
+        let mut uf = (0..n).collect::<Vec<UnionFindItem>>();
         for edge in edges {
-            let u = edge[0];
-            if u < 0 {
-                unreachable!("Invalid negative edge: {:?}", edge);
-            }
-            let u = u as UnionFindItem;
-            let v = edge[1];
-            if v < 0 {
-                unreachable!("Invalid negative edge: {:?}", edge);
-            }
-            let v = v as UnionFindItem;
-            uf.union(u, v);
+            let a_start = edge[0] as UnionFindItem;
+            let a_end = find(&mut uf, a_start);
+            let b_start = edge[1] as UnionFindItem;
+            let b_end = find(&mut uf, b_start);
+            uf[a_start as usize] = b_end;
+            uf[a_end as usize] = b_end;
         }
-        uf.find(source) == uf.find(destination)
+        find(&mut uf, source as UnionFindItem) == find(&mut uf, destination as UnionFindItem)
     }
 }
 
