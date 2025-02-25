746308d8c759919c30f884914d0a4642538717a5
4onen <mattjdupree@gmail.com>
2025-02-24 20:52:46 -0800

    Add solution for number_of_islands

--- challenges/number_of_islands/number_of_islands.rs
+++ challenges/number_of_islands/number_of_islands.rs
@@ -72,6 +72,37 @@ impl Solution {
     }
 }
 
+// Try filling in with 0s on fastest sol'n (4ms slower)
+// impl Solution {
+//     pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
+//         fn dfs(i: i16, j: i16, grid: &mut [Vec<char>]) {
+//             if i < 0
+//                 || j < 0
+//                 || i >= grid.len() as i16
+//                 || j >= grid[i as usize].len() as i16
+//                 || grid[i as usize][j as usize] != '1'
+//             {
+//                 return;
+//             }
+//             grid[i as usize][j as usize] = '\0';
+//             dfs(i - 1, j, grid);
+//             dfs(i + 1, j, grid);
+//             dfs(i, j - 1, grid);
+//             dfs(i, j + 1, grid);
+//         }
+//         let mut islands = 0;
+//         for i in 0..grid.len() {
+//             for j in 0..grid[i].len() {
+//                 if grid[i][j] == '1' {
+//                     islands += 1;
+//                     dfs(i as i16, j as i16, &mut grid);
+//                 }
+//             }
+//         }
+//         islands
+//     }
+// }
+
 #[cfg(test)]
 mod tests {
     use super::*;
